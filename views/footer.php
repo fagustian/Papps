ddd5eec3c653cfdeb1ad94e0a43adf73468cf3f5
fagustian <agustian.fuji@gmail.com>
2018-01-28 13:39:50 +0700

    inisiasi Papps

---
+++ views/footer.php
@@ -0,0 +1,11 @@
+    
+    <footer class="footer">
+      <div class="container">
+          <p class="text-muted">&copy; Fuji Agustian 2018</p>
+      </div>
+    </footer>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.jsjquery-slim.min.js"></script>
+
+  </body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+ 
+    
+  </head>
+  <body>
+
+  <nav id="myNavbar" class="navbar navbar-light bg-faded">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#" style="font-family:serif;font-style:italic;">Papps</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="nav-item"><a class="nav-link" href="#">Your timeline</a></li>
+      <li class="nav-item"><a class="nav-link" href="#">Your status</a></li>
+      <li class="nav-item"><a class="nav-link" href="#">Public</a></li> 
+      <li class="nav-item"><a class="nav-link" href="#">Profiles</a></li> 
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#" class="btn signUpButton">Sign Up</a></li>
+    </ul>
+  </div>
+</nav> 
